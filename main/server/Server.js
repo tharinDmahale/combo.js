92506af52b22060e8abb0cb3e51a5250c0661362
thmhlk <tharin.mahale@ifs.com>
2025-06-09 15:09:13 +0530

    server configs

--- main/server/Server.js
+++ main/server/Server.js
@@ -1,32 +1,18 @@
 "use strict";
 
-import path from "path";
-import { fileURLToPath } from "url";
 import express from "express";
-import favicon from "serve-favicon";
-import Cors from "./middleware/Cors.js";
 import Router from "./middleware/Router.js";
 import Configurations from "./middleware/Configurations.js";
 
 class Server {
-    static #dirname = path.dirname(fileURLToPath(import.meta.url));
-
     static async start() {
         const app = express();
-        const protocol = process.env.PROTOCOL;
-        const host = process.env.HOST;
-        const serverPort = process.env.SERVER_PORT;
-        const clientPort = process.env.CLIENT_PORT;
-        const cors = Cors.getCors(protocol, host, clientPort);
-        const format = express.json();
-        const staticObjects = express.static(path.join(Server.#dirname, "./public"));
-        const faviconObject = favicon(path.join(Server.#dirname, "./public/favicon.ico"));
+        const serverUrl = Configurations.setup(app);
 
-        Configurations.setup(app, format, cors, staticObjects, faviconObject);
         Router.initialize(app);
 
-        app.listen(serverPort, host, () => {
-            console.info(`Server is running at ${protocol}://${host}:${serverPort}`);
+        app.listen(process.env.SERVER_PORT, process.env.HOST, () => {
+            console.info(`Server is running at ${serverUrl}`);
         });
     }
 }
--- main/server/middleware/Configurations.js
+++ main/server/middleware/Configurations.js
@@ -1,8 +1,21 @@
 "use strict";
 
+import path from "path";
+import { fileURLToPath } from "url";
+import express from "express";
+import favicon from "serve-favicon";
+import Cors from "./Cors.js";
+
 class Configurations {
-    static setup(app, format, cors, staticObjects, favicon) {
-        app.use(format);
+    static setup(app) {
+        const dirname = path.dirname(fileURLToPath(import.meta.url));
+        const cors = Cors.getCors(
+            process.env.PROTOCOL, 
+            process.env.HOST, 
+            process.env.CLIENT_PORT
+        );
+
+        app.use(express.json());
 
         app.use((req, res, next) => {
             res.setHeader(cors.origin.header, cors.origin.value);
@@ -18,14 +31,16 @@ class Configurations {
             }
         });
 
-        app.use(staticObjects);
+        app.use(express.static(path.join(dirname, "../public")));
 
-        app.use(favicon);
+        app.use(favicon(path.join(dirname, "../public/favicon.ico")));
 
         app.use((req, res, next) => {
             res.setHeader("X-Favicon", "/favicon.ico");
             next();
         });
+
+        return cors.origin.value;
     }
 }
 
